b4d9db0bc1423c969ef304b7e0ef1db6f96463a2
chou <13642238156@163.com>
2020-09-24 10:15:17 +0800

    finish login

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import '../src/assets/fonts/iconfont.css'
+// 导入全局样式表
+import '../src/assets/css/global.css'
+
+import axios from 'axios'
 
 Vue.config.productionTip = false
+// 设置axios默认地址
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 把axios挂载到http上
+Vue.prototype.$http = axios
 
 new Vue({
   router,
---
+++ src/views/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class=''>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        logout(){
+            // 清空sessionstorage
+            window.sessionStorage.clear()
+            // 跳转登陆页面
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
